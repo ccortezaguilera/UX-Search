72bec6013375dd2fafd971eee2df0c33d74f0020
Martino Kuan <mjkuan@dons.usfca.edu>
2017-02-27 14:01:27 -0800

    Add the search box back to the top of the page for POST. 1. Force a write of the searchbox before the displayimage on event. 2. Change whitespace of displaysearchbox to spaces.

--- displaysearchbox.js
+++ displaysearchbox.js
@@ -3,24 +3,24 @@ var fs = require('fs');
 //var express = require('express');
 /*Server will handle inital page display and submit button.*/
 function displaySearch() {
-	http.createServer(function(request, response) {
-		if (request.url === "/") {
-			fs.readFile("index.html", function(err, data) {
-			if (err) {
-				console.log(err);
-				response.writeHead(404, {'Content-Type': 'text/html'});
-				response.end();
-			} else {
-            	response.writeHead(200, {'Content-Type': 'text/html'});
-				response.write(data.toString());
-            	response.end();
-			}
-		});
-		} 
-		else {
-			response.writeHead(404);
-			response.end("FILE NOT FOUND");
-		}
-	}).listen(5070);
+    http.createServer(function(request, response) {
+        if (request.url === "/") {
+            fs.readFile("index.html", function(err, data) {
+                if (err) {
+                    console.log(err);
+                    response.writeHead(404, {'Content-Type': 'text/html'});
+                    response.end();
+                } else {
+                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.write(data.toString());
+                    response.end();
+                }
+            });
+        } 
+        else {
+            response.writeHead(404);
+            response.end("FILE NOT FOUND");
+        }
+    }).listen(5070);
 }
 module.exports = displaySearch;

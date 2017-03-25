b1c7e0a6fdfb9a25d7e559154914132d2c78fefc
mjkuan <mjkuan@dons.usfca.edu>
2017-03-24 17:28:48 -0700

    Use cheerio to write POST response. 1. Remove most indexOfs and difficult to follow writes with JQuery modification with Cheerio and a single write at the end.

--- displayimage.js
+++ displayimage.js
@@ -14,7 +14,7 @@ const imagesPerPage = 100;
  * @param {string} query 
  */
 function writeToClientResponse(response, body, query) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(body.slice(0, body.indexOf("</body>")));
     parseSearchQuery(query, response);
 }
@@ -22,10 +22,11 @@ function writeToClientResponse(response, body, query) {
 /**
  * Parses the search query and write the images to the HTML page.
  * 
+ * @param {CheerioStatic} $
  * @param {string} query 
  * @param {http.ServerResponse} response 
  */
-function parseSearchQuery(query, response) {
+function parseSearchQuery($, query, response) {
     // Prevent undefined query from being used to mess with the server
     if (query === undefined || query === null) {
         response.end();
@@ -47,8 +48,8 @@ function parseSearchQuery(query, response) {
         port: "80"
     };
 
-    http.get(options, function(mrEdmondResponse) {
-        sendRequestToMrEdmond(mrEdmondResponse, response);
+    http.get(options, function (mrEdmondResponse) {
+        sendRequestToMrEdmond($, mrEdmondResponse, response);
     });
 }
 
@@ -57,25 +58,22 @@ function parseSearchQuery(query, response) {
  * formatted document. Then, write all the thumbnails to the response to the
  * client.
  * 
+ * @param {CheerioStatic} $ 
  * @param {http.ServerResponse} mrEdmondResponse 
  * @param {http.ServerResponse} clientResponse 
  */
-function sendRequestToMrEdmond(mrEdmondResponse, clientResponse) {
+function sendRequestToMrEdmond($, mrEdmondResponse, clientResponse) {
     var body = "";
-    mrEdmondResponse.on("data", function(data) {
+    mrEdmondResponse.on("data", function (data) {
         body += data;
     });
     console.log(body);
-    mrEdmondResponse.on("end", function() {
+    mrEdmondResponse.on("end", function () {
         console.log("Entered MrEdmondResponseEnd");
+        
         // Write images
         let thumbnailList = getThumbnails(body);
-        //TODO will fix the response to write the file and place value with search query
-        //clientResponse.write("<html><body>"); 
-        for (var thumbnail of thumbnailList) {
-            clientResponse.write(thumbnail);
-        }
-        clientResponse.write("\n</div>");
+        $('#imageDiv').append(thumbnailList);
 
         // Write page numbers
 
@@ -90,34 +88,17 @@ function sendRequestToMrEdmond(mrEdmondResponse, clientResponse) {
         //     pageNumber = String(pageCount);
         // }
 
-        let pageBoxHtml = fs.readFileSync("PageInputBox.txt", "utf8");
-
-        // Locate the HTML page display markup.
-        // let positionOfImageEnd = pageBoxHtml.indexOf("<label for=\"PageNumber\"");
-        let positionOfPageInput = pageBoxHtml.indexOf(`id="pageNumber"`);
-        let positionBeforeMaxPage = pageBoxHtml.indexOf(`id="maxPageNumber">`) + `id="maxPageNumber">`.length;
-        // let positionOfDivEnd = pageBoxHtml.indexOf("</div>", positionOfPageInput);
-
-        // Write HTML up to the point where the page number is shown.
-        clientResponse.write(pageBoxHtml.substring(0, positionOfPageInput));
-
         // Write the page number into the input box.
-        // clientResponse.write("<input type=\"text\" name=\"PageNumber\" id=\"pageNumber\" ");
-        clientResponse.write(`value="${pageNumber}" `);
-        clientResponse.write(`size="${Math.floor(Math.log10(pageCount)) + 1}" `);
-        clientResponse.write(`maxlength="${Math.floor(Math.log10(pageCount)) + 2}" `);
-        clientResponse.write(pageBoxHtml.substring(positionOfPageInput, positionBeforeMaxPage));
+        var pageNumberBox = $('#pageNumber');
+        pageNumberBox.val(pageNumber);
+        pageNumberBox.attr('size', Math.floor(Math.log10(pageCount)) + 1);
+        pageNumberBox.attr('maxlength', Math.floor(Math.log10(pageCount)) + 2);
 
         // Write out the max page number
-        clientResponse.write(`${pageCount}`);
+        $('#maxPageNumber').text(`${pageCount}`);
 
-        // Write out the rest of the HTML.
-        clientResponse.write(pageBoxHtml.substring(positionBeforeMaxPage));
-
-        // clientResponse.write(pageBoxHtml.substring(positionOfPageInput, positionOfDivEnd + "</div>".length));
-        // clientResponse.write("<input type=\"submit\" name=\"Submit\" value=\"Go!\"></form>");
-        // clientResponse.write(pageBoxHtml.substring(positionOfDivEnd + "</div>".length));
         console.log("Ending prior to use");
+        clientResponse.write($.html());
         clientResponse.end();
     });
 }
@@ -163,13 +144,13 @@ function getNumberOfResults(response) {
 var fs = require('fs');
 var url = require('url');
 var querystring = require('querystring');
-var jsdom = require('jsdom');
+var cheerio = require('cheerio');
 function display_image() {
-    http.createServer(function(request, response) {
+    http.createServer(function (request, response) {
         if (request.method === "POST") {
             if (request.url == "/") {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                request.on('data', function(data) {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                request.on('data', function (data) {
                     // Obtain the query information.
                     let formData = querystring.parse(data.toString());
                     let encodedQuery = encodeURIComponent(formData['SearchQuery']);
@@ -183,47 +164,46 @@ function display_image() {
 
                     // Read the HTML file and select key positions in the file.
                     webHtmlPage = fs.readFileSync("index.html").toString();
-                    let positionOfTextBoxInput = webHtmlPage.indexOf(`id="query"`);
-                    let positionOfBodyEnd = webHtmlPage.indexOf(`<div align="left" id="imageDiv">`, positionOfTextBoxInput) + `<div align="left" id="imageDiv">`.length;
+                    var $ = cheerio.load(webHtmlPage);
 
-                    // Write the search box and the orginal query in the box.
-                    response.write(webHtmlPage.substring(0, positionOfTextBoxInput));
-                    response.write(`value="${formData['SearchQuery']}"`);
-                    response.write(webHtmlPage.substring(positionOfTextBoxInput, positionOfBodyEnd));
+                    // Add the orginal query back to the box.
+                    if (formData['SearchQuery']) {
+                        $('#query').val(formData['SearchQuery']);
+                    }
 
                     console.log(encodedQuery);
-                    parseSearchQuery(encodedQuery, response);
+                    parseSearchQuery($, encodedQuery, response);
                 });
-                
-            /*
-            fs.readFile(path.substring(1), function(err, data) {
-                if (err) {
-                    console.log(err);
-                    console.log("couldn't process request!")
-                    response.writeHead(404, {'Content-Type': 'text/html'});
-                    response.end();
-                } else {
-                    writeToClientResponse(response, data.toString(), queries);
-                }
-            });*/
-        }
-        else {
-            fs.readFile("index.html", function(err, data) {
-                if (err) {
-                    console.log(err);
-                    console.log("We entered here because there was extra and we couldn't open file");
-                    response.writeHead(404, {'Content-Type': 'text/html'});
-                    response.end();
-                } else {
-                    response.writeHead(200, {'Content-Type': 'text/html'});
-                    response.write(data.toString());
-                    response.end();
-                }
-            });
-        }
+
+                /*
+                fs.readFile(path.substring(1), function(err, data) {
+                    if (err) {
+                        console.log(err);
+                        console.log("couldn't process request!")
+                        response.writeHead(404, {'Content-Type': 'text/html'});
+                        response.end();
+                    } else {
+                        writeToClientResponse(response, data.toString(), queries);
+                    }
+                });*/
+            }
+            else {
+                fs.readFile("index.html", function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        console.log("We entered here because there was extra and we couldn't open file");
+                        response.writeHead(404, { 'Content-Type': 'text/html' });
+                        response.end();
+                    } else {
+                        response.writeHead(200, { 'Content-Type': 'text/html' });
+                        response.write(data.toString());
+                        response.end();
+                    }
+                });
+            }
 
         }
-        
+
     }).listen(8888);
 }
 module.exports = display_image;
